30900d1a2185f38215f94b5c87b5150ec1fe7626
Linda Zou <lindashzou@gmail.com>
2023-05-13 17:01:07 -0500

    Added a function to detect if the ray hit the sphere in main

--- Raytracing Tutorial/main.cpp
+++ Raytracing Tutorial/main.cpp
@@ -3,8 +3,19 @@
 #include "vec3.h"
 
 #include <iostream>
+bool hit_sphere(const point3& center, double radius, const ray& r) {
+	vec3 oc = r.origin() - center;
+	auto a = dot(r.direction(), r.direction());
+	auto b = 2.0 * dot(oc, r.direction());
+	auto c = dot(oc, oc) - radius * radius;
+	auto discriminant = b * b - 4 * a * c;
+	return (discriminant > 0);
+}
 
 color ray_color(const ray& r) {
+	if (hit_sphere(point3(0, 0, -1), 0.5, r)) {
+		return color(1, 0, 0);
+	}
 	vec3 unit_direction = unit_vector(r.direction());
 	auto t = 0.5 * (unit_direction.y() + 1.0);
 	return (1.0 - t) * color(1.0, 1.0, 1.0) + t * color(0.5, 0.7, 1.0);
@@ -12,7 +23,7 @@ color ray_color(const ray& r) {
 
 int main() {
 	// Image
-	const auto aspect_ratio = 16.0 / 9 / 0;
+	const auto aspect_ratio = 16.0 / 9.0;
 	const int image_width = 400;
 	const int image_height = static_cast<int>(image_width / aspect_ratio);
 
