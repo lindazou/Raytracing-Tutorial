8f7455dfd10be79f2ec0692d2c6e775b3b5bcb1e
Linda Zou <lindashzou@gmail.com>
2023-05-20 21:02:30 -0500

    Added a random number function

--- Raytracing Tutorial/rtweekend.h
+++ Raytracing Tutorial/rtweekend.h
@@ -4,6 +4,7 @@
 #include <cmath>
 #include <limits>
 #include <memory>
+#include <cstdlib>
 
 
 // Usings
@@ -18,6 +19,15 @@ const double infinity = std::numeric_limits<double>::infinity();
 const double pi = 3.1415926535897932385;
 
 // Utility Functions
+inline double random_double() {
+    // Returns a random real in [0, 1)
+    return rand() / (RAND_MAX + 1.0);
+}
+
+inline double random_double(double min, double max) {
+    // Returns a random real in [min, max)
+    return min + (max - min) * random_double();
+}
 
 inline double degrees_to_radians(double degrees) {
     return degrees * pi / 180.0;
