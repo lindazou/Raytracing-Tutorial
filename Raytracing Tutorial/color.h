8f8d1da309a3e1199238d70abaaea038546acfc9
Linda Zou <lindashzou@gmail.com>
2023-05-20 21:17:47 -0500

    Altering the color class and main to include the new camera and to calculate the color differently

--- Raytracing Tutorial/color.h
+++ Raytracing Tutorial/color.h
@@ -5,7 +5,17 @@
 
 #include <iostream>
 
-void write_color(std::ostream& out, color pixel_color) {
+void write_color(std::ostream& out, color pixel_color, int samples_per_pixel) {
+    auto r = pixel_color.x();
+    auto g = pixel_color.y();
+    auto b = pixel_color.z();
+
+    // Divide the color by the number of samples
+    auto scale = 1.0 / samples_per_pixel;
+    r *= scale;
+    g *= scale;
+    b *= scale;
+
     // Write the translated [0,255] value of each color component.
     out << static_cast<int>(255.999 * pixel_color.x()) << ' '
         << static_cast<int>(255.999 * pixel_color.y()) << ' '
