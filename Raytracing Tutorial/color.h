ed20a115bbb5e21d580092dd8f6956fb49a0c342
Linda Zou <lindashzou@gmail.com>
2023-05-20 21:33:08 -0500

    Fixing the color.h class so the values will clamp

--- Raytracing Tutorial/color.h
+++ Raytracing Tutorial/color.h
@@ -17,9 +17,9 @@ void write_color(std::ostream& out, color pixel_color, int samples_per_pixel) {
     b *= scale;
 
     // Write the translated [0,255] value of each color component.
-    out << static_cast<int>(255.999 * pixel_color.x()) << ' '
-        << static_cast<int>(255.999 * pixel_color.y()) << ' '
-        << static_cast<int>(255.999 * pixel_color.z()) << '\n';
+    out << static_cast<int>(256 * clamp(r, 0.0, 0.999)) << ' '
+        << static_cast<int>(256 * clamp(g, 0.0, 0.999)) << ' '
+        << static_cast<int>(256 * clamp(b, 0.0, 0.999)) << '\n';
 }
 
 #endif
