0998f565acffc24647b0b786be40b29c3d43c5f5
Linda Zou <lindashzou@gmail.com>
2023-05-20 21:11:03 -0500

    Added a camera class and a clamp function in rtweekend.h

--- Raytracing Tutorial/rtweekend.h
+++ Raytracing Tutorial/rtweekend.h
@@ -19,6 +19,18 @@ const double infinity = std::numeric_limits<double>::infinity();
 const double pi = 3.1415926535897932385;
 
 // Utility Functions
+inline double clamp(double x, double min, double max) {
+    if (x < min) {
+        return min;
+    }
+
+    if (x > max) {
+        return max;
+    }
+
+    return x;
+}
+
 inline double random_double() {
     // Returns a random real in [0, 1)
     return rand() / (RAND_MAX + 1.0);
